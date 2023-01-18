e526d2fd91d8ac5fb0656c279f9c14b2c6198ec1
Marcelo Amador <marcelojaf@gmail.com>
2023-01-18 17:05:59 -0300

    ZXing is working

--- src/BarcodeScanApp/BarcodeScanApp/Views/ZXingPage.xaml.cs
+++ src/BarcodeScanApp/BarcodeScanApp/Views/ZXingPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace BarcodeScanApp.Views
@@ -10,6 +10,24 @@ namespace BarcodeScanApp.Views
         public ZXingPage()
         {
             InitializeComponent();
+
+            btnScan.Clicked += async (sender, e) =>
+            {
+
+#if __ANDROID__
+	// Initialize the scanner first so it can track the current context
+	MobileBarcodeScanner.Initialize (Application);
+#endif
+
+                var scanner = new ZXing.Mobile.MobileBarcodeScanner();
+
+                var result = await scanner.Scan();
+
+                if (result != null)
+                {
+                    txtScannedContent.Text = result.Text;
+                }
+            };
         }
     }
 }
